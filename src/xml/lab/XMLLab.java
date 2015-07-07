9b9efdd3fc281d72b90136f44b79485b9d40942c
Kevin Barahona <kbarahona@localhost.localdomain>
2015-07-07 03:30:19 -0600

    NGRAM Tetas

--- src/xml/lab/XMLLab.java
+++ src/xml/lab/XMLLab.java
@@ -18,6 +18,7 @@ import com.sun.syndication.io.SyndFeedInput;
 import com.sun.syndication.io.XmlReader;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 import org.jdom.Element;
 
 /**
@@ -26,14 +27,22 @@ import org.jdom.Element;
  */
 public class XMLLab {
 
+    private static ArrayList<String> GoodComments;
+    private static ArrayList<String> BadComments;
+    private static int GoodCounter = 0;
+    private static int BadCounter = 0;
+
     /**
      * @param args the command line arguments
      * @throws java.lang.Exception
      */
     public static void main(String[] args) throws Exception {
+        GoodComments = new ArrayList();
+        BadComments = new ArrayList();
 
         URL url = new URL("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=10/xml");
         HttpURLConnection httpcon = (HttpURLConnection) url.openConnection();
+
         // Reading the feed
         SyndFeedInput input = new SyndFeedInput();
         SyndFeed feed = input.build(new XmlReader(httpcon));
@@ -61,16 +70,48 @@ public class XMLLab {
         for (SyndEntry x : entries) {
             SyndContentImpl syc = (SyndContentImpl) x.getContents().get(0);
             String value = syc.getValue();
+
             List<Element> foreign = (List<Element>) x.getForeignMarkup();
             for (Element y : foreign) {;
                 if (y.getName().equals("rating")) {
+                    int rating = Integer.parseInt(y.getValue());
+                    if (rating < 3) {
+                        BadCounter++;
+                        BadComments.add(value);
+                    } else {
+                        GoodCounter++;
+                        GoodComments.add(value);
+                    }
                     System.out.println(value);
-                    System.out.println(y.getValue());
+                }
+            }
+
+        }
+    }
+
+    public static String[] NGram(String s, int len) {
+        String[] parts = s.split(" ");
+        String[] result = null;
+        if (parts.length - len + 1 < 0) {
+            result = new String[parts.length - len + 1];
+
+            for (int i = 0; i < parts.length - len + 1; i++) {
+                StringBuilder sb = new StringBuilder();
+                for (int k = 0; k < len; k++) {
+                    if (k > 0) {
+                        sb.append(' ');
+                    }
+                    sb.append(parts[i + k]);
                 }
 
             }
         }
+        return result;
+    }
 
+    public static void printNGram(String Comment) {
+        String[] NGram = NGram(Comment, 3);
+        
     }
 
 }
