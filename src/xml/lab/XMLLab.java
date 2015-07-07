a3d207455faf041a7adf555594c1419284f3383f
Ricardo <ricardo.ortega@unitec.edu>
2015-07-07 02:39:57 -0600

    Popeye ya hay entries

--- src/xml/lab/XMLLab.java
+++ src/xml/lab/XMLLab.java
@@ -9,11 +9,14 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Iterator;
 import java.util.List;
- 
+
 import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.io.SyndFeedInput;
 import com.sun.syndication.io.XmlReader;
+import java.net.MalformedURLException;
+import org.jdom.Element;
+
 /**
  *
  * @author Daemon
@@ -26,21 +29,37 @@ public class XMLLab {
      */
     public static void main(String[] args) throws Exception {
         URL url = new URL("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=10/xml");
-        HttpURLConnection httpcon = (HttpURLConnection)url.openConnection();
+        HttpURLConnection httpcon = (HttpURLConnection) url.openConnection();
         // Reading the feed
         SyndFeedInput input = new SyndFeedInput();
         SyndFeed feed = input.build(new XmlReader(httpcon));
         List entries = feed.getEntries();
         Iterator itEntries = entries.iterator();
- 
+
         while (itEntries.hasNext()) {
-            SyndEntry entry = (SyndEntry)itEntries.next();
-            String mod = entry.getLink().substring(25);
-            String[] arreglito = mod.split("/");
-            String[] arreglito2 = arreglito[3].split("\\?");
-            String ID = arreglito2[0].substring(2);
-            System.out.println(ID);
+            SyndEntry entry = (SyndEntry) itEntries.next();
+            String list = entry.getLink().split("/")[6].split("\\?")[0].substring(2);
+            System.out.println("ID: " + list);
+            getIDRSS(list);
+            System.out.println("-------------------------------------------");
         }
     }
-    
+
+    public static void getIDRSS(String id) throws Exception {
+        URL url = new URL("http://ax.itunes.apple.com/us/rss/customerreviews/id=" + id + "/sortBy=mostRecent/xml");
+        HttpURLConnection httpcon = (HttpURLConnection) url.openConnection();
+        // Reading the feed
+        SyndFeedInput input = new SyndFeedInput();
+        SyndFeed feed = input.build(new XmlReader(httpcon));
+        List<SyndEntry> entries = feed.getEntries();
+        for (SyndEntry x : entries) {
+            List<Element> foreign = (List<Element>) x.getForeignMarkup();
+            for (Element y : foreign) {
+                if (y.getName().equals("rating")) {
+                    System.out.println(y.getValue());
+                }
+            }
+        }
+    }
+
 }
